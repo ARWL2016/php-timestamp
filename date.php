fc53487458c7f56238020c0cf8095debcdadbe47
Alistair Willis <alistairrwillis@gmail.com>
2018-05-07 17:54:52 +0100

    finish project

--- date.php
+++ date.php
@@ -1,38 +1,72 @@
 <?php
-    $timestamp = '';
-    $message = '';
-    $date = '';
-    $maxTimestamp = 2147483648;
-    $ts = 0;
-
-
-    // get input from request 
-    $input = isset($_GET['input']) ? trim($_GET['input']) : '';
-
-    // check if it is a number
-    $isNum = is_numeric($input);
-
-    if ($isNum) {
-        if ($input < $maxTimestamp) {
-            $date = date("Y-m-d H:i:s", (int)$input);
-            $timestamp = $input;
-        } else {
-            $message = 'Max timestamp exceeded';
-        }
-        
-
-    } else {
-        // try to convert input to unix timestamp
-        $ts = strtotime($input);
-
-        if ($ts) {   
-            $timestamp = $ts;
-            $date = date("Y-m-d H:i:s", $timestamp);
-        } else {
-            $message = 'Error. Invalid input.';
-        }
     
+class Date {
+    public static $date = '';
+    public static $timestamp = '';
+    public static $message = '';
+    private static $input = '';
+    const MAX_TIMESTAMP = 2147483648; // 2038 problem
+
+    public static function processInput() {
+
+        self::setInputFromUri();
+
+        if (self::$input == '') {
+            self::useDefault();
+            return;
+        }
+
+        // check if it is a number
+        $isNum = is_numeric(self::$input);
+
+        if ($isNum) 
+            self::processInputAsNumber();
+        else
+            self::processInputAsDateString();
     }
+
+    private static function processInputAsDateString() {
+        // try to convert input to unix timestamp
+        $unix = strtotime(self::$input);
+
+        if ($unix) {   
+            self::$timestamp = $unix;
+            self::setDateFromTimestamp(self::$timestamp);
+        } else {
+            self::setMessage("Sorry. Your input was invalid.");
+        }
+    }
+
+    private static function processInputAsNumber() {
+        if (self::$input < self::MAX_TIMESTAMP) {
+            self::setDateFromTimestamp(self::$input);
+            self::$timestamp = self::$input;
+        } else {
+            self::setMessage("Maximum timestamp exceeded.");
+        }
+    }
+
+    private static function useDefault() {
+        self::$timestamp = time();
+        self::setDateFromTimestamp();
+    }
+
+    private static function setInputFromUri() {
+        self::$input = isset($_GET['input']) ? strip_tags(trim($_GET['input'])) : '';
+    }
+
+    private static function setDateFromTimestamp() {
+        self::$date = date("Y-m-d H:i:s", (int)self::$timestamp);
+    }
+
+    private static function setMessage(string $string) {
+        self::$message = $string;
+    }
+    
+
+}
+
+    
 
    
 
--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 <?php 
-
-require 'date.php';
-    
+    require 'date.php';
+    Date::processInput();
 ?>
 
 <!DOCTYPE html>
@@ -9,35 +8,41 @@ require 'date.php';
 <head>
     <meta charset='utf-8'>
     <title>Timestamp Microservice</title>
+    <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href= "assets/styles.css" >
 </head>
 <body>
     <div>
         <h1>Timestamp Microservice</h1>
-        <p>Unix time is standard for measuring time used in many operating systems. It is defined as a number of seconds since <time>Thursday 1 January, 1970</time>.</p> 
-        <p>Enter a unix timestamp or a date in the format 'December 30 2001'.</p>
+        <p>
+            Unix time is a standard for measuring time used in many operating systems. 
+            It is defined as the number of seconds since the <time>1st January 1970</time>
+            and can represent datetimes up until the <time>19th January 2038</time>.
+        </p> 
+        <p>You can enter either a unix timestamp or a date query such as '3 January 2000' or 'next week'.</p>
         
-
         <form id="form" action="index.php" method="GET">
             <input type="text" name="input" id="input" placeholder="Enter a date or unix timestamp"> 
-            <input type="submit" id="submit" value="Convert"> 
+            <input type="submit" id="submit" class="submit-btn" value="Convert"> 
         </form>
 
-        <p>date:
-        <?php echo $date ?>
+        <?php if (Date::$date != ''): ?>
+            <p><strong>Date:</strong>
+                <?php echo Date::$date ?>
+            </p>
+        <?php endif ?>
+
+        <?php if (Date::$timestamp != ''): ?>
+            <p><strong>Timestamp:</strong>
+                <?php echo Date::$timestamp ?>
+            </p>
+        <?php endif ?>
+
+        <?php if (Date::$message != ''): ?>
+        <p class="error-msg">
+            <?php echo Date::$message ?>
         </p>
-        <p>timestamp:
-            <?php echo $timestamp ?>
-        </p>
-        <p>error:
-            <?php echo $message ?>
-        </p>
-        <p>ts:
-            <?php echo $ts ?>
-        </p>
-        <p>isNum:
-            <?php echo $isNum ?>
-        </p>
+        <?php endif ?>
     </div>
 </body>
 </html>
